35607ed78c430dd81f2ee7365bc52275740ca74e
P@ko <pako332@protonmail.com>
2021-08-10 23:53:05 -0700

    edited

--- grafana/run_grafana.sh
+++ grafana/run_grafana.sh
@@ -2,6 +2,6 @@
 
 docker run --rm \
     --mount type=bind,source=$PWD/custom.ini,target=/etc/grafana/grafana.ini\
-    --mount type=bind,source=$PWD/RootCA.key,target=/etc/grafana/RootCA.key\
-    --mount type=bind,source=$PWD/RootCA.crt,target=/etc/grafana/RootCA.crt\
+    --mount type=bind,source=$PWD/privkey.pem,target=/etc/grafana/privkey.pem\
+    --mount type=bind,source=$PWD/cert.pem,target=/etc/grafana/cert.pem\
     -p 5000:5000 grafana/grafana
