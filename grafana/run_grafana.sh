8313b566c241a674101bde9f0b0b9fbace9474e9
P@ko <pako332@protonmail.com>
2021-08-11 02:27:42 -0700

    Update run_grafana.sh

--- grafana/run_grafana.sh
+++ grafana/run_grafana.sh
@@ -4,4 +4,5 @@ docker run --rm \
     --mount type=bind,source=$PWD/custom.ini,target=/etc/grafana/grafana.ini\
     --mount type=bind,source=$PWD/privkey.pem,target=/etc/grafana/privkey.pem\
     --mount type=bind,source=$PWD/cert.pem,target=/etc/grafana/cert.pem\
+    --mount type=bind,source=$PWD/data,target=/var/lib/grafana\
     -p 5000:5000 grafana/grafana
